b7089a567d6bd7c8e51ce951722ad44b787f7525
Felipe Jardim <felipejardim95@gmail.com>
2022-01-30 17:03:35 -0400

    inserido conteudo da aula 2

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,10 @@
 import { Box, Button, Text, TextField, Image } from '@skynexui/components'
+import {useState} from 'react';
+import {useRouter} from 'next/router'
+
 import appConfig from '../config.json'
 
-function GlobalStyle() {
-  return (
-    <style global jsx>{`
-      * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-        list-style: none;
-      }
-      body {
-        font-family: 'Open Sans', sans-serif;
-      }
-      /* App fit Height */ 
-      html, body, #__next {
-        min-height: 100vh;
-        display: flex;
-        flex: 1;
-      }
-      #__next {
-        flex: 1;
-      }
-      #__next > * {
-        flex: 1;
-      }
-      /* ./App fit Height */ 
-    `}</style>
-  );
-}
+
 
 function Title(props) {
   const Tag = props.tag || "h1"
@@ -44,11 +20,11 @@ function Title(props) {
 }
 
 export default function PaginaInicial() {
-  const username = 'felipejardim';
+  const [username, setUsername] = useState('felipejardim');
+  const roteamento = useRouter();
 
   return (
     <>
-      <GlobalStyle />
       <Box
         styleSheet={{
           display: 'flex', alignItems: 'center', justifyContent: 'center',
@@ -75,6 +51,10 @@ export default function PaginaInicial() {
           {/* Formulário */}
           <Box
             as="form"
+            onSubmit={e=>{
+              e.preventDefault();
+              roteamento.push("/chat")
+            }}
             styleSheet={{
               display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
               width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
@@ -95,6 +75,8 @@ export default function PaginaInicial() {
                   backgroundColor: appConfig.theme.colors.neutrals[800],
                 },
               }}
+              value={username}
+              onChange={e => setUsername(e.target.value)}
             />
             <Button
               type='submit'
